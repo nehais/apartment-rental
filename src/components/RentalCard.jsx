9124c4f2ee55c0fdaa835936ecd6bf27e31d8cce
Neha Sinha <nehasinha82@gmail.com>
2024-11-02 16:35:58 +0100

    Page Not Found image & Rental Card with CSS

--- src/components/RentalCard.jsx
+++ src/components/RentalCard.jsx
@@ -4,6 +4,9 @@ function RentalCard({apartment}) {
   return (
     <div className="rental-card">
       <img src={apartment.picture_url} alt="" className="rent-picture"/>
+      <h3>{apartment.name}</h3>
+      <p>Hosted at {apartment.host_location}</p>
+      <h4>{apartment.price} per night</h4>      
     </div>
   );
 }
