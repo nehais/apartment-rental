f7d5ab5521ecb2878df5672587c0f87d5b59c216
Neha Sinha <nehasinha82@gmail.com>
2024-11-07 21:13:02 +0100

    Page Not Found - CSS

--- src/components/PageNotFound.jsx
+++ src/components/PageNotFound.jsx
@@ -6,7 +6,9 @@ const PageNotFound = () => {
     <div className="notfound">
       <img src="./src/assets/page-not-found.png" />
       <h3>Uh-oh! Looks like this page went on a vacation.</h3>
-      <Link to="/pagenotfound"></Link>
+      <Link to="/" id="back-home">
+        Take me Home
+      </Link>
     </div>
   );
 };
