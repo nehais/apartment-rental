32269ef7534bc2b01dca87c04c3c1464edafd049
Neha Sinha <nehasinha82@gmail.com>
2024-11-08 14:01:38 +0100

    About page CSS

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,23 +1,32 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import gitHubLogo from "../assets/GitHub-logo.png";
+import linkedInLogo from "../assets/LinkedIn-logo.png";
 
 const AboutUs = () => {
   return (
     <div className="about-us">
-      <h3>
-        Nest & Rest was developed to search an apartment for the destination
-      </h3>
+      <p>
+        <b>Nest & Rest:</b> Your one-stop destination for finding the perfect
+        apartment, tailored to your travel needs! <br />
+        Built by:
+      </p>
       <div className="about-user">
         <h4>Neha Sinha</h4>
-        <Link to="https://github.com/nehais/apartment-rental">
+        <Link to="https://github.com/nehais/">
           <img src={gitHubLogo} alt="Git Hub Logo" className="git-logo"></img>
+        </Link>
+        <Link to="https://linkedin.com/in/neha-builds">
+          <img src={linkedInLogo} alt="Git Hub Logo" className="in-logo"></img>
         </Link>
       </div>
       <div className="about-user">
         <h4>Teresa</h4>
         <Link to="https://github.com/nehais/apartment-rental">
           <img src={gitHubLogo} alt="Git Hub Logo" className="git-logo"></img>
+        </Link>
+        <Link to="https://linkedin.com/in/neha-builds">
+          <img src={linkedInLogo} alt="Git Hub Logo" className="in-logo"></img>
         </Link>
       </div>
     </div>
