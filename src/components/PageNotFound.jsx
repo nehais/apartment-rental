f3fbf1aea538236c73e98efe77ef36244024440e
Neha Sinha <nehasinha82@gmail.com>
2024-11-14 20:30:23 +0100

    Page Not Found image

--- src/components/PageNotFound.jsx
+++ src/components/PageNotFound.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import pageNotFound from "../assets/page-not-found.png";
 
 const PageNotFound = () => {
   return (
     <div className="notfound">
-      <img src="./src/assets/page-not-found.png" />
+      <img src={pageNotFound} />
       <h3>Uh-oh! Looks like this page went on a vacation.</h3>
       <Link to="/" id="back-home">
         Take me Home
